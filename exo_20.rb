e068e3d548bd45548b10f787186f8bfa9a8ff132
Yaniv <yanivou.bbb@gmail.com>
2020-01-14 17:14:23 +0100

    Ajout du 20

---
+++ exo_20.rb
@@ -0,0 +1,14 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+print ">"
+pyramide=gets.chomp.to_i
+
+puts "Voici la pyramide : "
+i = 1
+if i >= 1 && i <= 25
+
+       while i<=pyramide	
+	puts ("#" * i) 
+	i=i+1
+end
+end
+
